7def1677a0121334007663fd950e28073362a5e6
haneefullah00345 <HaneefUllah00345@gmail.com>
2023-09-30 18:07:01 +0500

    Cucumber

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,26 @@
 const { defineConfig } = require("cypress");
+const {
+  addCucumberPreprocessorPlugin,
+} = require("@badeball/cypress-cucumber-preprocessor");
+const {
+  preprocessor,
+} = require("@badeball/cypress-cucumber-preprocessor/browserify");
 
 module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter',
-  watchForFileChanges:false,
-  pageLoadTimeout:50000,
-  experimentalStudio:true,
-  chromeWebSecurity:false,
-  defaultCommandTimeout:50000,
-  screenshotOnRunFailure:true,
+  reporter: "cypress-mochawesome-reporter",
+  watchForFileChanges: false,
+  pageLoadTimeout: 50000,
+  experimentalStudio: true,
+  chromeWebSecurity: false,
+  defaultCommandTimeout: 50000,
+  screenshotOnRunFailure: true,
   e2e: {
-    experimentalRunAllSpecs:true,
-    setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on);
+    experimentalRunAllSpecs: true,
+    async setupNodeEvents(on, config) {
+      await addCucumberPreprocessorPlugin(on, config);
+
+      on("file:preprocessor", preprocessor(config));
+      require("cypress-mochawesome-reporter/plugin")(on);
       // implement node event listeners here
     },
     projectId: "ncf61t",
